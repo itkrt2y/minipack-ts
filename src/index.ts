da9d250bcdf531b728adf90085fda1d5963d1e6c
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-09-14 22:01:58 +0900

    Pass filename as an arg

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import ts from "typescript";
 import { createGraph } from "./graph";
 import { bundle } from "./bundler";
 
-const graph = createGraph("./examples/index.ts");
+const filename = process.argv[2];
+if (!filename) throw new Error("No filename provided");
+
+const graph = createGraph(filename);
 const result = bundle(graph);
 
 ts.sys.writeFile("dist/bundled.js", result);
