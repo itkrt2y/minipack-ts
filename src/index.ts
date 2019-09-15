97f23c6e3b299da8929ed356eed11a6ec641746c
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-09-15 17:42:56 +0900

    Fix error message

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { createGraph } from "./graph";
 import { bundle } from "./bundler";
 
 const filename = process.argv[2];
-if (!filename) throw new Error("No filename provided");
+if (!filename) throw new Error("No filename is provided");
 
 const graph = createGraph(filename);
 const result = bundle(graph);
